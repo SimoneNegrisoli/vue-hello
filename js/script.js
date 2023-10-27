dca78aa97f12cb3865a2ecba4eda37ce44769d54
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-10-27 15:34:39 +0200

    h1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        title: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
